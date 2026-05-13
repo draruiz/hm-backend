ee986ce9e9ee1f5d38215cbb01b03370d9848f07
Camilo Hernandez <brayan.hernandez1222@gmail.com>
2026-05-13 11:24:00 -0500

    cors

--- src/main.ts
+++ src/main.ts
@@ -6,44 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
-  // Enable CORS for localhost (HTTP and HTTPS)
+  // Enable CORS for all origins (development/testing only)
   app.enableCors({
-    origin: (origin, callback) => {
-      const allowedOrigins = [
-        // HTTP localhost
-        'http://localhost:3000',
-        'http://localhost:3001',
-        'http://localhost:4200',
-        'http://localhost:5173',
-        'http://localhost:8080',
-        'http://localhost:4321',
-        'http://127.0.0.1:3000',
-        'http://127.0.0.1:3001',
-        'http://127.0.0.1:4200',
-        'http://127.0.0.1:5173',
-        'http://127.0.0.1:8080',
-        'http://127.0.0.1:4321',
-
-        // HTTPS RAILWAY
-        'https://admin-healthymind-new-version.vercel.app',
-        'https://healthymind-newversion.vercel.app',
-      ];
-
-      // Permitir todos los subdominios de almaymente.io (con o sin www)
-      const almaymenteRegex = /^https:\/\/([\w-]+\.)?almaymente\.io$/;
-
-      if (
-        !origin ||
-        allowedOrigins.includes(origin) ||
-        almaymenteRegex.test(origin)
-      ) {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-        callback(null, true);
-      } else {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    origin: true,
     credentials: true,
   });
 
